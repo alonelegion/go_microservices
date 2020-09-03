90caf2f87a3e5f4601394a744230e1d4da8f1d26
AloneLegion <com.berdin@gmail.com>
2020-09-03 21:30:35 +0300

    add comments to currency files

--- currency/main.go
+++ currency/main.go
@@ -21,13 +21,17 @@ func main() {
 	// register the currency server
 	protos.RegisterCurrencyServer(gs, cs)
 
+	// register the reflection service which allows clients to determine the methods
+	// for this gRPC service
 	reflection.Register(gs)
 
+	// create a TCP socket for inbound server connections
 	l, err := net.Listen("tcp", ":8082")
 	if err != nil {
 		log.Error("Unable to listen", "error", err)
 		os.Exit(1)
 	}
 
-	gs.Serve(l)
+	// listen for requests
+	_ = gs.Serve(l)
 }
